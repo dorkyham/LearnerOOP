d94901fe92f5bbfd8a6139602a05cb6f86b7f428
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-05-15 12:23:01 +0700

    Initial commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Annisa Nabila Nasution on 15/05/19.
+//  Copyright © 2019 Annisa Nabila Nasution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
