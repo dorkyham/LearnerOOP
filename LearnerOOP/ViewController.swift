f0702aec269d7075197dada497456a820e26d9b2
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-05-15 14:19:14 +0700

    Passes CD10 & CD17

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var learnerNameLabel: UILabel!
+    
+    @IBOutlet weak var learnerAgeLabel: UILabel!
+    
+    @IBOutlet weak var learnerGenderLabel: UILabel!
+    
+    var learnerInstance : LearnerModel? //ini buat sebuah object dari class LearnerModel
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        learnerInstance = LearnerModel.init(nameLearner: "Annisa Nabila Nasution", ageLearner: 19, genderLearner: "Other", imageProfileLearner: "")
+        
+            updateUI()
     }
 
-
+    func updateUI(){
+        //unwrap optional biar ilangin tanda tanya
+        if let instance = learnerInstance {
+            learnerNameLabel.text = instance.name
+            
+            learnerAgeLabel.text = "\(instance.age)"
+            
+            learnerGenderLabel.text = instance.gender
+        }
+        
+    }
+    
+    
+    @IBAction func increaseAgeButtonClicked(_ sender: UIButton) {
+        if let instance = learnerInstance{
+            instance.increaseAge()
+        }
+        updateUI()
+    }
 }
 
